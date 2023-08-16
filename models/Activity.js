64654758b361d5c2d6d2de3c66377b60de0d2dcf
Diego <dgerman359@gmail.com>
2023-08-16 12:13:46 -0300

    users1

--- models/Activity.js
+++ models/Activity.js
@@ -2,7 +2,7 @@ import { model,Schema,Types } from "mongoose";
 
 let collection = "activities"
 let schema = new Schema({
-    name: {type:String, required: true},
+    name: {type:String, required:true},
     photo: { type:String,required:true },
     itinerary_id: {type:Types.ObjectId,required:true,ref:'itineraries'}
 
